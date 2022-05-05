32b24d26936de232eeacd8d63b71e1f66b0cccd4
buddhdev-jay <jay.b@simformsolutions.com>
2022-05-05 17:37:55 +0530

    Onboarding screen issue fixed

--- IOS_UI_Design/IOS_UI_Design/OnBoardScreen/OnBoardingViewController.swift
+++ IOS_UI_Design/IOS_UI_Design/OnBoardScreen/OnBoardingViewController.swift
@@ -12,56 +12,57 @@ class OnBoardingViewController: UIViewController, UICollectionViewDelegate {
     @IBOutlet weak var skipButton: SkipUIButton!
     @IBOutlet weak var nextButton: NextUIButton!
     @IBOutlet weak var collectionView: UICollectionView!
-    
     @IBOutlet weak var pageControl: UIPageControl!
+    
+
     var pages = [Page]()
-    var currentPage = 0 {
+    var current = 0 {
         didSet {
-            pageControl.currentPage = currentPage
-            if(currentPage != pages.count - 1) {
-                nextButton.setTitle("Next", for: .normal)
-                skipButton.isHidden = false
-               
-            } else {
+            pageControl.currentPage = current
+            if(current == pages.count - 1) {
                 nextButton.setTitle("Explore", for: .normal)
                 skipButton.isHidden = true
-                
-
+            } else {
+                nextButton.setTitle("Next", for: .normal)
+                skipButton.isHidden = false
             }
-           
+     
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         collectionView.dataSource = self
         collectionView.delegate = self
-        collectionView.register(UINib(nibName: "OnboardingCollectionViewCell", bundle: Bundle.main),
-                                forCellWithReuseIdentifier: "OnboardingCollectionViewCell")
-        pageControl.numberOfPages = pages.count
+        collectionView.register(UINib(nibName: "OnboardingCollectionViewCell", bundle: Bundle.main),forCellWithReuseIdentifier: "OnboardingCollectionViewCell")
         setData()
+       pageControl.numberOfPages = pages.count
+        
         
     }
+}
+
+
+extension OnBoardingViewController {
+    
     @IBAction func onNextButtonClick(_ sender: NextUIButton) {
-        if (currentPage == pages.count - 1){
+        if (current == pages.count - 1) {
             alert(customMessage: "End of Pages")
         }
         else {
-            currentPage += 1
-            let indexPath = IndexPath(item: currentPage, section: 0)
+            current += 1
+            let indexPath = IndexPath(item: current, section: 0)
             collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
         }
-        
     }
     
-    @IBAction func pageChanged(_ sender: Any) {
-        let pageControl = sender as! UIPageControl
-        collectionView.scrollToItem(at: IndexPath(item:pageControl.currentPage, section: 0),
-                                    at: .centeredHorizontally, animated: true)
-        
+    @IBAction func pageChanged(_ sender: UIPageControl) {
+        collectionView.scrollToItem(at: IndexPath(item:sender.currentPage, section: 0),at: .centeredHorizontally, animated: true)
     }
-    
+}
+
+// MARK: - set Data
+extension OnBoardingViewController {
     
     func setData(){
         pages.append(Page(imageName:"onBoardImageone", title: "Search your job", description: "Figure out your top five priorities whether it is company culture, salary."))
@@ -87,17 +88,14 @@ extension OnBoardingViewController : UICollectionViewDataSource {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let width = scrollView.frame.width
-        currentPage = Int(scrollView.contentOffset.x/width)
-        
+        current = Int(scrollView.contentOffset.x/width)
     }
     
 }
 
 extension OnBoardingViewController : UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let collectionHeight = collectionView.bounds.height
-        let cellWidth = UIScreen.main.bounds.width
-        return CGSize(width: cellWidth, height: collectionHeight)
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
